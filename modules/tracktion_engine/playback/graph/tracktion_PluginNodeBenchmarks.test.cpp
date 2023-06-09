b83af69429e21ea8de407c86259ca4708edb21ac
Daniel Gergely <gergely.daniel1@gmail.com>
2023-06-09 17:28:24 +0200

    Benchmarks:   Add benchmark for preparing a large Edit with Send, Return, and Patchbay plugins

--- modules/tracktion_engine/playback/graph/tracktion_PluginNodeBenchmarks.test.cpp
+++ modules/tracktion_engine/playback/graph/tracktion_PluginNodeBenchmarks.test.cpp
@@ -40,6 +40,7 @@ public:
 
         auto& engine = *tracktion::engine::Engine::getEngines()[0];
         runNodePreparationBenchmarks (engine);
+        runLargeGraphUpdateBenchmark (engine);
     }
 
 private:
@@ -86,6 +87,76 @@ private:
                                                                               cnp.sampleRate, cnp.blockSize);
         }
     }
+
+    void runLargeGraphUpdateBenchmark (Engine& engine)
+    {
+        constexpr int trackCount = 200;
+        auto edit = Edit::createSingleTrackEdit (engine);
+
+        for (int trackIndex = 0; trackIndex < trackCount; ++trackIndex)
+        {
+            auto audioTrack = edit->insertNewAudioTrack (TrackInsertPoint {nullptr, nullptr}, nullptr);
+            const juce::StringArray pluginTypesToAdd{
+                AuxReturnPlugin::xmlTypeName,
+                PatchBayPlugin::xmlTypeName,
+                AuxReturnPlugin::xmlTypeName,
+                PatchBayPlugin::xmlTypeName,
+                VolumeAndPanPlugin::xmlTypeName,
+                VolumeAndPanPlugin::xmlTypeName,
+                VolumeAndPanPlugin::xmlTypeName,
+                AuxSendPlugin::xmlTypeName,
+                PatchBayPlugin::xmlTypeName,
+                VolumeAndPanPlugin::xmlTypeName,
+                VolumeAndPanPlugin::xmlTypeName,
+                VolumeAndPanPlugin::xmlTypeName,
+                AuxSendPlugin::xmlTypeName,
+                PatchBayPlugin::xmlTypeName,
+            };
+
+            for (auto pluginType : pluginTypesToAdd)
+            {
+                auto plugin = edit->getPluginCache().createNewPlugin (pluginType, {});
+                jassert (plugin.get() != nullptr);
+
+                const int commonBusNumber = 1000;
+
+                if (auto returnPlugin = dynamic_cast<AuxReturnPlugin*>(plugin.get())) {
+                    returnPlugin->busNumber.setValue(trackIndex == 0 ? commonBusNumber : trackIndex, nullptr);
+                }
+                if (auto sendPlugin = dynamic_cast<AuxSendPlugin*>(plugin.get())) {
+                    // Sending all sends to the same return is approximately 3x more time compared to sending each to
+                    // a different return:
+                    // sendPlugin->busNumber.setValue(trackIndex, nullptr);
+                    sendPlugin->busNumber.setValue(commonBusNumber, nullptr);
+                }
+
+                audioTrack->pluginList.insertPlugin (plugin, -1, nullptr);
+            }
+        }
+
+        {
+            tracktion::graph::PlayHead playHead;
+            tracktion::graph::PlayHeadState playHeadState { playHead };
+            ProcessState processState { playHeadState, edit->tempoSequence };
+            CreateNodeParams cnp { processState, 44100.0, 256 };
+
+            // Repeating the benchmark is helpful when profiling, so we see the benchmarked workload, not the setup.
+            // If profiling is not a concern, the iteration can be removed.
+            // for (int i = 0; i < 50; ++i)
+            {
+                auto editNode = createNodeForEdit (*edit, cnp);
+
+                const ScopedBenchmark sb (createBenchmarkDescription (
+                    "Node Preparation",
+                    juce::String ("Preparing 123 tracks").replace ("123", juce::String (trackCount)).toStdString(),
+                    juce::String ("Preparing with AuxSend, AuxReturn and PatchBay Plugins").toStdString()));
+
+                [[ maybe_unused ]] auto graph = node_player_utils::prepareToPlay (std::move (editNode), nullptr,
+                                                                                cnp.sampleRate, cnp.blockSize);
+            }
+        }
+    }
+
 };
 
 static PluginNodeBenchmarks pluginNodeBenchmarks;
